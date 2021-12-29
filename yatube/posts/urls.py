433c41c9dd589ff98b900e4a83cb1fd697a00160
Artur Avtukhov <archy6754267@gmail.com>
2021-12-29 20:46:08 +0500

    Save_004

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -5,6 +5,6 @@ app_name = 'posts'
 urlpatterns = [
     # Главная страница
     path('', views.index, name='index'),
-    path('group/<slug:slug>/', views.group_posts, name='group_post_list'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
 
 ]   
