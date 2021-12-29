1be6d8d6d8c27f69a560cb7ff81243c1a732a566
Artur Avtukhov <archy6754267@gmail.com>
2021-12-29 19:29:41 +0500

    Save_001

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
 from django.http import HttpResponse
+from .models import Post, Group
 
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+#def index(request):    
+#    template = loader.get_template('posts/index.html')
+#    title = "Это главная страница проекта Yatube"
+#    context = {
+#        'title': title
+#              }
+#    return HttpResponse(template.render(context, request)) 
+
+def index(request):
+    title = "Это главная страница проекта Yatube"
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = group.title
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': title
+    }
+    return render(request, 'posts/group_list.html', context) 
 
 # view-функция принимает параметр pk из path()
-def group_posts(request, slug):
-    return HttpResponse(f'Пост номер {slug}') 
+#def group_posts(request, slug):
+#    template = loader.get_template('posts/group_list.html')
+
+#    group = get_object_or_404(Group, slug=slug)
+
+#    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+#    context = {
+#        'group': group,
+#        'posts': posts,
+#    }
+#    return HttpResponse(template.render(context, request)) 
+
+
+
+
+
+
+
+
+
+    
